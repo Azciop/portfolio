b3090225a9cdf9eb1dbe591576d8106c408b259a
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-08-30 19:27:24 +0200

    30082022 commit

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -1,11 +1,11 @@
 // Copy to clipboard function
 function copyURI(evt) {
-    evt.preventDefault();
-    navigator.clipboard.writeText(evt.target.getAttribute('href')).then(() => {
-      console.log('link copied succesfully')
-    }, () => {
-      
-    });
+  evt.preventDefault();
+  navigator.clipboard.writeText(evt.target.getAttribute('href')).then(() => {
+    console.log('link copied succesfully')
+  }, () => {
+
+  });
 }
 
 // Show copy to clipboard message function 
@@ -17,14 +17,14 @@ function showMessage(evt) {
 // Hide copy to clipboard message function 
 window.setTimeout("hideMessage();", 5000);
 
-function hideMessage(){
-document.getElementById("copied_message").style.display=" none";
+function hideMessage() {
+  document.getElementById("copied_message").style.display = " none";
 }
 
 // Scroll Up Button Function 
 scrollUpBtn = document.getElementById("scrollUpBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -36,6 +36,30 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function scrollUp() {
-  document.documentElement.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
   document.body.scrollTop = 0;
 }
+
+// SlideIn Skill animation
+function debounce(func, wait = 20, imediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!imediate) func.apply(context.args);
+    };
+    var callNow = imediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  console.count(e);
+}
+
+window.addEventListener('scroll', debounce(checkSlide, 100));
