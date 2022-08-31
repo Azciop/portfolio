db9878a8eb5aceac90477959e599dfeb7917f405
Steven Bernamont <stevenbernamontcontact@gmail.com>
2022-08-31 15:06:07 +0200

    added links and loading animation

--- front/javascript/script.js
+++ front/javascript/script.js
@@ -40,26 +40,3 @@ function scrollUp() {
   document.body.scrollTop = 0;
 }
 
-// SlideIn Skill animation
-function debounce(func, wait = 20, imediate = true) {
-  var timeout;
-  return function () {
-    var context = this, args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!imediate) func.apply(context.args);
-    };
-    var callNow = imediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
-
-const sliderImages = document.querySelectorAll('.slide-in');
-
-function checkSlide(e) {
-  console.count(e);
-}
-
-window.addEventListener('scroll', debounce(checkSlide, 100));
